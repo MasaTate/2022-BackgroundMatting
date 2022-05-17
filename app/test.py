70cb13caf1d9093923fa461f83354f1b497e8c4b
舘野将寿 <tatemasaaac@gmail.com>
2022-05-18 00:49:54 +0900

    changed test.py

--- app/test.py
+++ app/test.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 from PIL import Image
 import argparse
+from torchvision.models.segmentation.deeplabv3 import ASPP
 
 loader = transforms.Compose([transforms.ToTensor()])
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -18,9 +19,10 @@ def main(content_path):
     print(tensor_img.shape)
 
     model = resnet.ResNet50()
-    output = model(tensor_img)
-    for x in output:
-        print(x.shape)
+    x0,x1,x2,x3,x4 = model(tensor_img)
+    aspp = ASPP(2048,[3,6,9])
+    x4 = aspp(x4)
+    print(x4.shape)
 
 
 if __name__ == "__main__":
