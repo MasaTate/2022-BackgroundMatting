b872dcab3b1c7e7844a545d417a264f1df00dcdd
MasaTate <tatemasaaac@gmail.com>
2022-05-25 10:19:55 +0900

    start train_base.py

---
+++ app/train_base.py
@@ -0,0 +1,14 @@
+from email.mime import image
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from PIL import Image
+import argparse
+
+def main():
+    return
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_name")
+    main()
